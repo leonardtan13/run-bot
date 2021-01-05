79690e06f035b5e1cb22d141f59e6e09b2111169
leonardtan13 <leonardtan13@gmail.com>
2021-01-05 22:36:03 +0800

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+
+const TelegramBot = require('node-telegram-bot-api');
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/start/, (msg) => {
+	bot.sendMessage(msg.chat.id, "hello dory");
+})
